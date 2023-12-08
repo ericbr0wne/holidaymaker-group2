75203f44ec9315e2a01c84060dc368635814f944
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2023-12-08 14:03:25 +0100

    Database connection and tables
    Created the database connection in c# project.
    Added code for creating tables and relations.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Npgsql;
+
+string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=holidaymaker";
+
+await using var db = NpgsqlDataSource.Create(dbUri);
+
+await using (var cmd = db.CreateCommand(@"
+CREATE TABLE IF NOT EXISTS room(
+    id SERIAL,
+    number INTEGER NOT NULL,
+    size INTEGER NOT NULL,
+    location INTEGER NOT NULL,
+    price INTEGER NOT NULL,
+    PRIMARY KEY(id)
+)"))
+{
+    await cmd.ExecuteNonQueryAsync(); 
+}
+
+await using (var cmd = db.CreateCommand(@"
+CREATE TABLE IF NOT EXISTS booking(
+    id SERIAL,
+    number INTEGER NOT NULL,
+    customer_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
+    start_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    end_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    extra_bed BOOLEAN NOT NULL DEFAULT FALSE,
+    half_board BOOLEAN NOT NULL DEFAULT FALSE,
+    full_board BOOLEAN NOT NULL DEFAULT FALSE,
+    price INTEGER NOT NULL,
+    PRIMARY KEY(id)
+)"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand(@"
+CREATE TABLE IF NOT EXISTS customer(
+    id SERIAL,
+    first_name CHARACTER VARYING(255) NOT NULL,
+    last_name CHARACTER VARYING(255) NOT NULL,
+    mail CHARACTER VARYING(255),
+    phone INTEGER,
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY(id)
+)"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand(@"
+CREATE TABLE IF NOT EXISTS location(
+    id SERIAL,
+    name CHARACTER VARYING(255) NOT NULL,
+    rating SMALLINT NOT NULL,
+    beach_distance INTEGER NOT NULL,
+    centre_distance INTEGER NOT NULL,
+    pool BOOLEAN NOT NULL DEFAULT FALSE,
+    kids_club BOOLEAN NOT NULL DEFAULT FALSE,
+    restaurant BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY(id)
+)"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand(@"
+CREATE TABLE IF NOT EXISTS add_on(
+    type CHARACTER VARYING(255) NOT NULL,
+    price INTEGER NOT NULL
+)"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand(@"
+ALTER TABLE IF EXISTS room
+    ADD FOREIGN KEY(location) REFERENCES location(id)    
+"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand(@"
+ALTER TABLE IF EXISTS booking
+    ADD FOREIGN KEY(room_id) REFERENCES room(id),
+    ADD FOREIGN KEY(customer_id) REFERENCES customer(id)
+"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
--- obj/Debug/net8.0/holidaymaker-group2.AssemblyInfo.cs
+++ obj/Debug/net8.0/holidaymaker-group2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("holidaymaker-group2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c17e2992df2ab6653c0d413874daa08bedf1d90")]
 [assembly: System.Reflection.AssemblyProductAttribute("holidaymaker-group2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("holidaymaker-group2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
