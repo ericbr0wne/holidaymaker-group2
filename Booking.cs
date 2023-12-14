8b71951bf1c90e14895a28f2e0ca05c3989e3bf6
Szemenkar <johannes.szemenkar@hotmail.com>
2023-12-14 05:13:33 +0100

    Created function for booking. Have not considered if the booking already exists on that room that date.

---
+++ Booking.cs
@@ -0,0 +1,145 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace holidaymaker_group2;
+
+public class Booking(NpgsqlDataSource db)
+{
+    public async Task CreateBooking()
+    {
+        await using (var cmd = db.CreateCommand())
+        {
+            int customerNumber = 0;
+            bool foundCustomer = false;
+            
+            // Gjorde en while-loop för att hitta kunden (för och efternamnet) som man söker på och skriva ut denna+customer_id
+            // sedan sparas customer_id i variabeln "customerNumber"
+            // är inte klar då jag vill hitta ett sätt att lösa om man råkar trycka enter (så att allt inte kraschar)
+            while (!foundCustomer)
+            {
+                Console.Write("Enter customer's first and last name: ");
+                string? customerName = Console.ReadLine();
+                string[] customerNameSplit = customerName.Split(" ");
+
+                string qSearchCustomer = @$"
+                    SELECT c.first_name, c.last_name, c.customer_id
+                    FROM customers c
+                    WHERE c.first_name = '{customerNameSplit[0]}' AND c.last_name = '{customerNameSplit[1]}'
+                ";
+
+                var reader = await db.CreateCommand(qSearchCustomer).ExecuteReaderAsync();
+
+                if (await reader.ReadAsync())
+                {
+                    Console.WriteLine("Customer name: " + reader.GetString(0) + " " + reader.GetString(1) + "\nCustomer ID: " + (customerNumber = reader.GetInt32(2)));
+                    foundCustomer = true;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Customer not found, please try again.\n");
+                }
+            }
+
+            // Min tanke kring room_id är att vi hämtar den från en sökning i en annan klass (så att det faktiskt är möjligt att boka rummet det datumet)
+            Console.Write("\nEnter the room ID for this vacation: ");
+            string? stringRoomID = Console.ReadLine();
+            int roomID = int.Parse(stringRoomID);
+
+            // Snodde den snygga try-catchen från Klas, men la till while-loopar så att man måste skriva korrekt datum
+            string pattern = "yyyy-mm-dd";
+            DateTime sDate;
+            DateTime eDate;
+            string? stringStartDate = string.Empty;
+            string? stringEndDate = string.Empty;
+            bool enterDate1 = false;
+
+            while (!enterDate1)
+            {
+                Console.WriteLine("\nEnter the start date for this vacation ('yyyy-mm-dd'):");
+                stringStartDate = Console.ReadLine();
+                try
+                {
+                    sDate = DateTime.ParseExact(stringStartDate, pattern, null);
+                    enterDate1 = true;
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("{0} is not in the correct format", stringStartDate);
+                }
+            }
+            DateTime startDate = DateTime.Parse(stringStartDate);
+
+            bool enterDate2 = false;
+
+            while (!enterDate2)
+            {
+                Console.WriteLine("Enter the end date for this vacation ('yyyy-mm-dd'):");
+                stringEndDate = Console.ReadLine();
+                try
+                {
+                    eDate = DateTime.ParseExact(stringEndDate, pattern, null);
+                    enterDate2 = true;
+                }
+                catch (FormatException)
+                {
+                    Console.Clear();
+                    Console.WriteLine("{0} is not in the correct format", stringEndDate);
+                }
+            }
+            DateTime endDate = DateTime.Parse(stringEndDate);
+
+            // Genererar bara ett random nummer för bokningen mellan 10 000 och 99 999
+            Random rnd = new Random();
+            int bookingNumber = 30777;
+
+            bool checkNumber = true;
+
+            while (checkNumber)
+            {
+                int count = 0;
+
+                string qSearchBookingNumber = @$"
+                SELECT COUNT(*)
+                FROM bookings b
+                WHERE b.number = {bookingNumber}
+            ";
+
+                var reader = await db.CreateCommand(qSearchBookingNumber).ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    count = reader.GetInt32(0);
+                }
+
+                if (count == 1)
+                {
+                    bookingNumber = rnd.Next(10000, 99999);
+                    Console.WriteLine("New number generated: " + bookingNumber);
+                }
+                else
+                {
+                    checkNumber = false;
+                }
+            }
+
+
+            cmd.CommandText = "INSERT INTO bookings (number, customer_id, room_id, start_date, end_date) VALUES ($1, $2, $3, $4, $5)";
+
+            cmd.Parameters.AddWithValue(bookingNumber);
+            cmd.Parameters.AddWithValue(customerNumber);
+            cmd.Parameters.AddWithValue(roomID);
+            cmd.Parameters.AddWithValue(startDate);
+            cmd.Parameters.AddWithValue(endDate);
+
+            await cmd.ExecuteNonQueryAsync();
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,5 +5,8 @@ const string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgr
 
 await using var db = NpgsqlDataSource.Create(dbUri);
 
-var tables = new Tables(db);
-await tables.CreateAll();
+// var tables = new Tables(db);
+// await tables.CreateAll();
+
+Booking booking = new(db);
+await booking.CreateBooking();
