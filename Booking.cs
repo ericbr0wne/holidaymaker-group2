2678e433f4e13357f50726b7c683feca5a056550
= <klas_hermansson@hotmail.com>
2023-12-19 11:01:31 +0100

    Removed commentary and cleaned up Search.cs

--- Booking.cs
+++ Booking.cs
@@ -70,17 +70,16 @@ namespace holidaymaker_group2
             List<int> allRooms = new List<int>();
             List<int> bookedRooms = new List<int>();
 
-            Console.WriteLine("\n########################################################################\n");
             var reader = await db.CreateCommand(searchquery).ExecuteReaderAsync();
 
-            var resultTable = new ConsoleTable("#", "Hotel", "Room No", "Room size", "Rating", "Distance to Beach", "Distance to city centre", "Price"); //Creating a table using NuGet-package ConsoleTables 
-            resultTable.Configure(o => o.EnableCount = false); // Removes annoying counter from displaying below the table.
+            var resultTable = new ConsoleTable("#", "Hotel", "Room No", "Room size", "Rating", "Distance to Beach", "Distance to city centre", "Price"); 
+            resultTable.Configure(o => o.EnableCount = false); 
 
-            var cartTable = new ConsoleTable("#", "Hotel", "Room No", "Room size", "Rating", "Distance to Beach", "Distance to city centre", "Price"); //Creating a table using NuGet-package ConsoleTables 
-            cartTable.Configure(o => o.EnableCount = false); // Removes annoying counter from displaying below the table.
+            var cartTable = new ConsoleTable("#", "Hotel", "Room No", "Room size", "Rating", "Distance to Beach", "Distance to city centre", "Price"); 
+            cartTable.Configure(o => o.EnableCount = false); 
 
             int i = 1;
-            while (await reader.ReadAsync()) //Gets the values from the database and adds it to rows in the table for search results.
+            while (await reader.ReadAsync()) 
             {
                 allRooms.Add(reader.GetInt32(7));
                 resultTable.AddRow(i, reader.GetString(0), reader.GetInt32(1), reader.GetInt32(2), $"{reader.GetInt32(3)}/5", $"{reader.GetInt32(4)}km", $"{reader.GetInt32(5)}km", $"{reader.GetDecimal(6)}$");
@@ -89,28 +88,27 @@ namespace holidaymaker_group2
 
             while (true)
             {
-                Console.WriteLine("\n########################################################################\n");
-                Console.WriteLine(resultTable); // prints the search results
+                Console.WriteLine(resultTable); 
                 Console.WriteLine("\n Booked Rooms");
                 Console.WriteLine(cartTable);
 
-                Console.WriteLine("Write the number of the room you'd like to add to your cart");   //menu for choices after search
+                Console.WriteLine("Write the number of the room you'd like to add to your cart");   
                 Console.WriteLine("Leave empty to return to previous menu and add any selected rooms to cart");
                 string input = Console.ReadLine() ?? string.Empty;
                 if (input == string.Empty)
                 {
-                    if (bookedRooms.Count == 0) //if there isn't any rooms added to the list this exits the task and returns a null value to cart.
+                    if (bookedRooms.Count == 0) 
                     {
                         return null;
                     }
                     else
                     {
-                        Cart cart = new Cart(bookedRooms, startdate, enddate); //If there are rooms added this creates an instance of the cart-class and returns that instance.
+                        Cart cart = new Cart(bookedRooms, startdate, enddate);
                         return cart;
                     }
                 }
 
-                if (int.TryParse(input, out int value) && value > 0 && value <= allRooms.Count && !bookedRooms.Contains(allRooms[value - 1])) //Makes sure the user input is a number that is in the list and that isn't already added to cart.
+                if (int.TryParse(input, out int value) && value > 0 && value <= allRooms.Count && !bookedRooms.Contains(allRooms[value - 1])) 
                 {
                     bookedRooms.Add(allRooms[value - 1]);
                     cartTable.AddRow(resultTable.Rows[value -1]);
@@ -119,6 +117,12 @@ namespace holidaymaker_group2
                 {
                     Console.WriteLine("Invalid input!\n Make sure you did't try to add a room that's already added or isn't on the list");
                     Console.ReadKey();
+                }
+
+                //Because of bug where console.clear doesn't clear console window
+                for (int j = 0; j < 40; j++)
+                {
+                    Console.WriteLine();
                 }
             }
         }
