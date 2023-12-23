24f7cf674b9de8be5bcb3fb44d552409b6a3518d
stoffeek <stoffeek@gmail.com>
2023-12-23 04:48:09 +0100

    booking and view / addons

--- Bookings.cs
+++ Bookings.cs
@@ -9,53 +9,111 @@ using NpgsqlTypes;
 
 namespace holidaymaker_group2;
 
-    public class Bookings(NpgsqlDataSource db)
+public class Bookings(NpgsqlDataSource db)
+{
+
+    public async Task DisplayAvaibleRooms()
     {
-        public async Task AddBooking()
+        await using (var cmd = db.CreateCommand())
         {
-                DisplayHotels();
+            int beachDistance = DistanceInput("Enter max distance to for beach(KM): ");
+            int centreDistance = DistanceInput("Enter max distance to the beach(KM):");
 
-                Console.WriteLine("Enter your start date ( yyyy-mm-dd )");
-                DateTime NSD = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("enter your end date ( yyyy-mm-dd )");
-                DateTime NED = DateTime.Parse(Console.ReadLine());
+            string query = SearchQuery(beachDistance, centreDistance);
+            ----
 
-            await using (var cmd = db.CreateCommand())
-            {
-                cmd.CommandText =
-                    "SELECT COUNT (*) Booking WHERE @NewStartDate < End_date AND @NewEndDate > Start_Date";
-                cmd.Parameters.AddWithValue("NewStartDate", NSD);
-                cmd.Parameters.AddWithValue("NewEndDate", NED);
+            bool HasPool = FacilityAddon("Do you want pool? y/n?");
+            bool HasEveningEntertainment = FacilityAddon("Evening Entertainment? y/n ");
+            bool Hasresturant = FacilityAddon("Resturant at the hotel?");
+            bool HasKidsClub = FacilityAddon("KidsClub ? y/n");
 
-                var overlapCheck = (int)await cmd.ExecuteScalarAsync();
+            query = SearchQuery(beachDistance, centreDistance, HasPool, HasEveningEntertainment, Hasresturant,
+                HasKidsClub);
 
-                if (overlapCheck == 0)
-                {
-                    Console.WriteLine("Your booking had been added");
-                }
-                else
-                {
-                    Console.WriteLine("This choice is not avaible for this date, Please try another one");
-                }
-            }
         }
 
-        public async Task DisplayHotels()
-        {
-            await using (var cmd = db.CreateCommand())
-            {
-                List<string> hotels = new List<string>();
-             
-              string qDisplayAvaible = @$"SELECT r.*
-                    FROM rooms r
-                    JOIN bookings b ON r.room_id = b.room_id
-                    WHERE b.booking_id IS NULL 
-                    AND r.location_id = @HotelID
-                    AND @StartDate < b.end_date
-                    AND @EndDate > b.start_date";
+    }
 
+
+
+    private int DistanceInput(string prompt)
+    {
+        int distance = 0;
+
+        while (true)
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input) || int.TryParse(input, out distance))
+            {
+                break;
             }
 
+            Console.WriteLine("Enter a valid distance!");
+            Thread.Sleep(1000);
+        }
+
+        return distance;
+    }
+
+    private string SearchQuery(int beachDistance, int centreDistance, bool HasPool, bool HasEveningEntertainment,
+        bool HasResturant, bool HasKidsClub)
+    {
+        StringBuilder sQuery =
+            new StringBuilder(
+                "SELECT l.name, l.rating, l.beach_distance, l.centre_distance, r.room, r.number, r.size, r.price FROM rooms r JOIN locatioins l USING(location_id)");
+        if (beachDistance > 0)
+        {
+            sQuery.AppendLine($"WHERE l.beach_distance < {beachDistance}");
+        }
+
+        if (sQuery.ToString().Contains("WHERE"))
+        {
+            sQuery.AppendLine($"AND l.centre_distance < {centreDistance}");
+
+        }
+        else
+        {
+            sQuery.AppendLine($"WHERE l.centre_distance < {centreDistance}");
+        }
+
+        if (HasPool)
+        {
+            FacilityAddon(ref sQuery, 1);
+        }
+
+        if (HasEveningEntertainment)
+        {
+            FacilityAddon(ref sQuery, 2);
+        }
+
+        if (HasResturant)
+        {
+            FacilityAddon(ref sQuery, 3);
+        }
+
+        if (HasKidsClub)
+        {
+            FacilityAddon(ref sQuery, 4);
+        }
+
+        return sQuery.ToString();
+
+    }
+
+    private void FacilityAddon(ref StringBuilder sQuery, int facilityId)
+    {
+        if (sQuery.ToString().Contains("WHERE"))
+        {
+            sQuery.AppendLine($"INTERSECT SELECT l.name, r.number, r.size, l.rating, l.beach_distance, l.centre_distance, r.price, r.room_id FROM rooms r JOIN locations l USING (location_id) JOIN locations_to_facilities ltf USING (location_id) WHERE ltf.facility_id = {facilityId}");");
+        }
+        else
+        {
+            sQuery.AppendLine(
+                $"WHERE EXISTS ( SELECT 1 FROM location_to_facilities ltf WHERE ltf.facilityid = {facilityId} AND ltf.location_id = l.location_id");
         }
     }
+
+}
 
